f3d337030e23336012650d83bca973bdc2daaf24
Rafael Sergio <rafa.sergio.02@live.com>
2020-02-25 16:00:31 -0300

    lista de tecnologias criada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import nubank from "./assets/nubank.png";
+import TechList from "./components/TechList";
 
 function App() {
-  return <img src={nubank} alt="nubank" />;
+  return <TechList />;
 }
 
 export default App;
---
+++ src/components/TechList.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class TechList extends React.Component {
+  state = {
+    newTech: "",
+    techs: ["Node.js", "ReactJS", "React Native"]
+  };
+
+  handleInputChange = e => {
+    this.setState({ ...this.state, newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={this.state.newTech}
+          onChange={this.handleInputChange}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default TechList;
